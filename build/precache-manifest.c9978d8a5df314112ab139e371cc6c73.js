887ad91374ae52d2911c0dd3cf6bbb55b5456dfd
saron <saronsurya01@gmail.com>
2021-07-02 02:51:23 -0700

    Keeper deploy

---
+++ build/precache-manifest.c9978d8a5df314112ab139e371cc6c73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d4eab79d8f149ec8d13f79204ec8a2",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "79010dbd451cbc4d89f0",
+    "url": "/Keeper/static/js/2.636ef6fa.chunk.js"
+  },
+  {
+    "revision": "328ab139003f4bdf3a4f",
+    "url": "/Keeper/static/js/main.77c755d1.chunk.js"
+  },
+  {
+    "revision": "624a019943f6d16a0069",
+    "url": "/Keeper/static/js/runtime-main.bbbc4cd4.js"
+  }
+]);
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [animi, setAnimi] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  function inClick() {
+    setAnimi(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note" autoComplete="off">
+        {animi && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          onClick={inClick}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={animi ? 3 : 1}
+        />
+
+        <Zoom in={animi}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
